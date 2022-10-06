55b7ec2828784d800f9c89e060449721290e0531
root <root@CENTOS.oezbgllz4kvedmrcejacaokloh.dxbx.internal.cloudapp.net>
2022-10-06 13:21:28 +0000

    Nodejs Installation

---
+++ Nodejs_centos/README.md
@@ -0,0 +1,12 @@
+Run `sudo yum install -y nodejs` to install Node.js 14.x and npm.
+
+You may run dnf if yum is not available:
+     sudo dnf install -y nodejs
+
+You may also need development tools to build native addons:
+     sudo yum install gcc-c++ make
+
+To install the Yarn package manager, run:
+
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum install yarn
---
+++ Nodejs_centos/nodejs-centos.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+echo "
+--------------------------------------------------
+Installation of Nodejs and Its Dependencies
+--------------------------------------------------
+Adding Nodejs Repository
+--------------------------------------------------
+"
+
+curl -sL https://rpm.nodesource.com/setup_14.x | bash -
+
+echo "
+--------------------------------------------------
+Installing Nodejs and gcc-c++
+--------------------------------------------------
+"
+
+sudo yum install -y nodejs
+
+sudo yum install gcc-c++
+
+sudo yum install make
+echo "
+--------------------------------------------------
+Installation of Yarn Package Manager
+--------------------------------------------------
+"
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum install yarn
+
