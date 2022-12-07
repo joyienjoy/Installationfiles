f5635b95864988290c4a8cfeb44909ae529078b0
joyienjoy <92083624+joyienjoy@users.noreply.github.com>
2022-12-07 13:31:36 +0530

    Update nodejs_ubuntu.sh

--- Nodejs_Install/nodejs_ubuntu.sh
+++ Nodejs_Install/nodejs_ubuntu.sh
@@ -10,7 +10,7 @@ echo "
 Installing Nodejs
 --------------------------------------------------
 "
-sudo apt install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && sudo apt-get install -y nodejs
 
 echo "
 --------------------------------------------------
