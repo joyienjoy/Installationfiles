0e5000e67f841d1c2f185c04e827c37f50eb70c7
joyienjoy <92083624+joyienjoy@users.noreply.github.com>
2023-02-27 16:14:46 +0530

    Update master.sh

--- K8S-Installation/K8S-Ubuntu/master.sh
+++ K8S-Installation/K8S-Ubuntu/master.sh
@@ -13,14 +13,19 @@ comment
 
 #Install HTTPS and Curl
 apt-get update
-apt-get install -y apt-transport-https ca-certificates curl
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
 cat << comment
 INSTAL DOCKER CONTAINER ENGINE
 comment
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-#Install Docker
-apt install docker.io
 cat << comment
 INSTALL KUBEADM, KUBELET AND KUBECTL
 comment
@@ -44,6 +49,7 @@ cat << comment
 SETTING UP FILE PERMISSIONS
 comment
 
+#
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
